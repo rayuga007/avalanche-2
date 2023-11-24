4cf058a2e048dbd272ed24a85e42ebea8d2d5ac4
rayuga007 <119921420+rayuga007@users.noreply.github.com>
2023-11-24 11:11:49 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,20 @@ import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
 
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
-export default function HomePage() {
+const formatDateTime = (date) => {
+  return {
+    date: date.toLocaleDateString(),
+    time: date.toLocaleTimeString(),
+  };
+};
+
+const HomePage = () => {
   const [account, setAccount] = useState(undefined);
   const [balance, setBalance] = useState(0);
   const [inputA, setInputA] = useState(0);
   const [inputB, setInputB] = useState(0);
   const [transactionStatus, setTransactionStatus] = useState("");
-  const [currentDate, setCurrentDate] = useState("");
-  const [currentTime, setCurrentTime] = useState("");
+  const [dateTime, setDateTime] = useState(formatDateTime(new Date()));
   const [transactionHistory, setTransactionHistory] = useState([]);
 
   const checkWallet = async () => {
@@ -30,42 +36,35 @@ export default function HomePage() {
     setBalance(currentBalance.toNumber());
   };
 
-  const deposit = async () => {
+  const executeTransaction = async (transactionFunction, amount, type) => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const contract = new ethers.Contract(contractAddress, atm_abi.abi, signer);
+    
     try {
-      const transaction = await contract.deposit(inputA, { value: inputA });
+      const transaction = await transactionFunction(contract, amount);
       await transaction.wait();
-      setTransactionStatus("Deposit successful");
+      setTransactionStatus(`${type} successful`);
       getBalance();
-      updateTransactionHistory("Deposit", inputA);
+      updateTransactionHistory(type, amount);
     } catch (error) {
-      setTransactionStatus("Deposit failed");
+      setTransactionStatus(`${type} failed`);
     }
   };
 
+  const deposit = async () => {
+    await executeTransaction((contract, amount) => contract.deposit(amount, { value: amount }), inputA, "Deposit");
+  };
+
   const withdraw = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const contract = new ethers.Contract(contractAddress, atm_abi.abi, signer);
-    try {
-      const transaction = await contract.withdraw(inputB);
-      await transaction.wait();
-      setTransactionStatus("Withdrawal successful");
-      getBalance();
-      updateTransactionHistory("Withdrawal", inputB);
-    } catch (error) {
-      setTransactionStatus("Withdrawal failed");
-    }
+    await executeTransaction((contract, amount) => contract.withdraw(amount), inputB, "Withdrawal");
   };
 
   const updateTransactionHistory = (type, amount) => {
     const newTransaction = {
       type,
       amount,
-      date: new Date().toLocaleDateString(),
-      time: new Date().toLocaleTimeString(),
+      ...formatDateTime(new Date()),
     };
     setTransactionHistory([newTransaction, ...transactionHistory]);
   };
@@ -76,9 +75,7 @@ export default function HomePage() {
 
     // Update date and time every second
     const interval = setInterval(() => {
-      const date = new Date();
-      setCurrentDate(date.toLocaleDateString());
-      setCurrentTime(date.toLocaleTimeString());
+      setDateTime(formatDateTime(new Date()));
     }, 1000);
 
     // Cleanup interval on component unmount
@@ -88,12 +85,12 @@ export default function HomePage() {
   return (
     <main className="container">
       <header>
-        <h1>Welcome to Your Ethereum Wallet</h1>
-        <h2>Account Owner: Faizan Khan</h2>
+        <h1>Welcome Faiz</h1>
+        <h2>Account Holder: Faizan Khan</h2>
       </header>
       {account ? (
         <div>
-          <p>Your Balance: {balance} ETH </p>
+          <p>Account Balance: {balance} ETH </p>
           <p>Wallet Address: {account}</p>
 
           <div className="button-container">
@@ -103,7 +100,9 @@ export default function HomePage() {
               value={inputA}
               onChange={(e) => setInputA(e.target.value)}
             />
-            <button onClick={deposit}>Deposit ETH</button>
+            <button className="green-button" onClick={deposit}>
+              Deposit ETH
+            </button>
           </div>
 
           <div className="button-container">
@@ -113,7 +112,9 @@ export default function HomePage() {
               value={inputB}
               onChange={(e) => setInputB(e.target.value)}
             />
-            <button onClick={withdraw}>Withdraw ETH</button>
+            <button className="blue-button" onClick={withdraw}>
+              Withdraw ETH
+            </button>
           </div>
         </div>
       ) : (
@@ -124,20 +125,33 @@ export default function HomePage() {
         <p>Transaction Status: {transactionStatus}</p>
       </div>
 
-      <div>
-        <h3>Current Date: {currentDate}</h3>
-        <h3>Current Time: {currentTime}</h3>
+      <div className="date-time-container">
+        <h3>Date: {dateTime.date}</h3>
+        <h3>Time: {dateTime.time}</h3>
       </div>
 
       <div>
         <h3>Transaction History</h3>
-        <ul>
-          {transactionHistory.map((transaction, index) => (
-            <li key={index}>
-              {transaction.type} {transaction.amount} ETH - {transaction.date} {transaction.time}
-            </li>
-          ))}
-        </ul>
+        <table>
+          <thead>
+            <tr>
+              <th>Type</th>
+              <th>Amount</th>
+              <th>Date</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactionHistory.map((transaction, index) => (
+              <tr key={index}>
+                <td>{transaction.type}</td>
+                <td>{transaction.amount} ETH</td>
+                <td>{transaction.date}</td>
+                <td>{transaction.time}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
       <style jsx>{`
@@ -159,12 +173,46 @@ export default function HomePage() {
           margin-right: 10px;
         }
 
-        button {
+        .green-button {
+          background-color: #4CAF50; /* Green background */
+          color: white;
           padding: 10px;
           border: none;
           cursor: pointer;
         }
+
+        .blue-button {
+          background-color: #2196F3; /* Blue background */
+          color: white;
+          padding: 10px;
+          border: none;
+          cursor: pointer;
+        }
+
+        table {
+          width: 100%;
+          border-collapse: collapse;
+          margin-top: 20px;
+        }
+
+        th,
+        td {
+          border: 1px solid #ffffff; /* White border */
+          padding: 8px;
+          text-align: left;
+        }
+
+        th {
+          background-color: #4CAF50; /* Green background for header */
+          color: white;
+        }
+
+        .date-time-container {
+          text-align: left;
+        }
       `}</style>
     </main>
   );
-}
+};
+
+export default HomePage;
